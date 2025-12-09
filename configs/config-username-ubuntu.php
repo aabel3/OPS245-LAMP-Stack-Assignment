b44a52719eac71f4299f7a23d6f46769e1712553
Avraham Abel <avraham.lewis.abel@gmail.com>
2025-12-09 16:05:34 -0500

    Create config-username-ubuntu.php

---
+++ configs/config-username-ubuntu.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'myblog');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'username');
+define('DB_HOST', 'localhost');
+define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+?>
