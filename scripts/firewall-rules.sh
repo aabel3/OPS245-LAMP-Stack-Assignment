5d70e46499d9d629d6b3ec7de435dfbfe00673dd
Avraham Abel <avraham.lewis.abel@gmail.com>
2025-12-09 15:55:23 -0500

    Add files via upload

--- scripts/firewall-rules.sh
+++ scripts/firewall-rules.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Allow HTTP traffic (port 80)
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+# Allow SSH traffic (port 22)
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Allow established connections
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow DNS for apt
+sudo iptables -A INPUT -p udp --dport 53 -j ACCEPT
+
+# Set default policy to DROP
+sudo iptables -P INPUT DROP
